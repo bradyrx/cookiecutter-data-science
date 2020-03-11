50190da2884fd541e0b38c64b19c28bced49f43d
Riley Brady <riley.brady@colorado.edu>
2020-03-11 13:46:09 -0600

    make click req

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/data/make_dataset.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+
+import click
 
 
 @click.command()
@@ -22,9 +22,5 @@ if __name__ == '__main__':
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
     main()
