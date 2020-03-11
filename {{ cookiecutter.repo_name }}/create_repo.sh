bb59322a6e067acd627bd1533b740e20b9902b65
Riley Brady <riley.brady@colorado.edu>
2020-03-11 13:53:23 -0600

    add travis

--- {{ cookiecutter.repo_name }}/create_repo.sh
+++ {{ cookiecutter.repo_name }}/create_repo.sh
@@ -5,7 +5,7 @@ curl -u '{{ cookiecutter.github_username }}' https://api.github.com/user/repos -
 # Link local repository to git
 git init
 git add *
-git add .gitignore .pre-commit-config.yaml
+git add .gitignore .pre-commit-config.yaml .travis.yml
 git commit -m 'first commit'
 git remote add origin git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.project_name.lower().replace(' ', '_') }}.git
 git push -u origin master
